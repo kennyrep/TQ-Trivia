22c1dac66f772761afbaed445528799576ff09f7
Kenny Audu <kennyomoh04@gmail.com>
2019-05-28 18:56:01 -0400

    Second Commit

--- app/src/main/java/com/example/tqtrivia/NotificationScreen.java
+++ app/src/main/java/com/example/tqtrivia/NotificationScreen.java
@@ -1,15 +1,21 @@
 package com.example.tqtrivia;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 import java.util.ArrayList;
 
@@ -19,6 +25,7 @@ public class NotificationScreen extends AppCompatActivity {
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private CollectionReference notificationRef = db.collection("notification");
     private RecyclerviewAdapter adapter;
+    private String TAG;
 
 
     @Override
@@ -26,10 +33,24 @@ public class NotificationScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notification_screen);
 
+
         setUpRecyclerView();
+
+        FirebaseMessaging.getInstance().subscribeToTopic("general")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        String msg = getString(R.string.msg_subscribed);
+                        if (!task.isSuccessful()) {
+                            msg = getString(R.string.msg_subscribe_failed);
+                        }
+                        Log.d(TAG, msg);
+                        Toast.makeText(NotificationScreen.this, "Great, It Worked!", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 
-    private void setUpRecyclerView(){
+    private void setUpRecyclerView() {
 
         Query query = notificationRef;
 
@@ -46,13 +67,13 @@ public class NotificationScreen extends AppCompatActivity {
     }
 
     @Override
-    protected void onStart(){
+    protected void onStart() {
         super.onStart();
         adapter.startListening();
     }
 
     @Override
-    protected void onStop(){
+    protected void onStop() {
         super.onStop();
         adapter.stopListening();
     }
